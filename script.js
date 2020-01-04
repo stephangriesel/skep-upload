146bd359ca6ac0881e44d206a674c11d54be22ee
Stephan <sgriesel@gmail.com>
2020-01-04 08:15:13 +0100

    done, working version

---
+++ script.js
@@ -0,0 +1,21 @@
+$("#upload_widget_opener").cloudinary_upload_widget(
+    {
+        cloud_name: "dvme554nj",
+        upload_preset: "qtzenltc",
+        // cropping: "server",
+        folder: "skep",
+        theme: "minimal",
+        stylesheet: ".drag_content { border: 4px solid red }",
+        multiple: true,
+        sources: ["local"],
+        button_class: "ud_button inline no_margin",
+        button_caption: "Upload",
+        thumbnails: ".feature_thumb",
+        thumbnail_transformation: { width: 100, height: 100, crop: "fit" },
+        resource_type: "image"
+    },
+    function (error, result) {
+        console.log(error, result);
+    }
+);
+
